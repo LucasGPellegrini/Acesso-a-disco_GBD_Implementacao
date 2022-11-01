2f0122f397121b7bf0b07d2f7623dd0ff578e54c
Lucas Guerreiro Pellegrini <92069804+LucasGPellegrini@users.noreply.github.com>
2022-11-01 16:43:33 -0300

    Update registro.h

--- Acesso-a-disco_GBD/registro.h
+++ Acesso-a-disco_GBD/registro.h
@@ -2,5 +2,10 @@
 #define REGISTRO_H_INCLUDED
 typedef struct registro reg;
 
+FILE * CREATE_HEAP_FILE(long int nro_registros);
+reg READ_RANDOM(FILE *arq, int nseq);
+FILE * ISRT_AT_END(FILE *arq);
+FILE * UPDATE_RANDOM(FILE *arq, int nseq, int novo_texto);
+FILE * DELETE_RANDOM(FILE *arq, int nseq);
 
 #endif // REGISTRO_H_INCLUDED
