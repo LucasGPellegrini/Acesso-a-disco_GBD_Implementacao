23ec7f58ee8358006b47014f8ed5f22343106358
ph-marra <71722227+ph-marra@users.noreply.github.com>
2022-11-02 13:41:06 -0300

    Add files via upload

---
+++ Acesso-a-disco_GBD/size.h
@@ -0,0 +1,13 @@
+#ifndef SIZE_H_INCLUDED
+#define SIZE_H_INCLUDED
+
+#define TEXT_SIZE 46
+
+typedef unsigned long long int memory_size_t;
+typedef unsigned long int entry_number_t;
+
+const memory_size_t K = 1024;
+const memory_size_t M = 1048576;
+const memory_size_t G = 1073741824;
+
+#endif
