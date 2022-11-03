62cc49cf38caca671d7fd389883b382fb091cddb
ph-marra <71722227+ph-marra@users.noreply.github.com>
2022-11-03 11:38:26 -0300

    Update size.h

--- Acesso-a-disco_GBD/size.h
+++ Acesso-a-disco_GBD/size.h
@@ -9,5 +9,6 @@ typedef unsigned long int entry_number_t;
 const memory_size_t K = 1024;
 const memory_size_t M = 1048576;
 const memory_size_t G = 1073741824;
+const ULONG_MAX = 818446744073709551615;
 
 #endif
